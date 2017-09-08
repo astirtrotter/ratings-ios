2ae025782c48ae8d8d2803d9c5060eea93efd7af
Yonis Larsson <yonis.larsson.biz@gmail.com>
2017-09-08 14:21:00 +0200

    initial table view with sample data

---
+++ Ratings/Ratings/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Ratings
+//
+//  Created by Yonis Larsson on 9/8/17.
+//  Copyright © 2017 yonislarsson. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    // MARK: - Properties
+    var name: String?
+    var game: String?
+    var rating: Int
+}
